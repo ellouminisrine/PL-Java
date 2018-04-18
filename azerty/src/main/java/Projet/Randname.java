1924a39714caba954a67d9ad3db12267c5cccbf7
Nisrine ELLOUMI <nel@softcomputing.com>
2018-04-18 14:54:58 +0200

    with postgres

--- azerty/src/main/java/Projet/Randname.java
+++ azerty/src/main/java/Projet/Randname.java
@@ -1,15 +1,17 @@
 package Projet;
 import java.util.Random;
+//import org.postgresql.pljava
 import org.apache.commons.lang3.RandomStringUtils;
 //import org.apache.hadoop.hive.ql.exec.UDF;
 
 public class Randname  {
 			
-			public String evaluate(String input) {
+	public static void main(String[] args) {
 			
 			Random rand = new Random();
 			int  n = rand.nextInt(20) + 5;	
+			String input= "Nisrine";
 			char[] possibleCaracter= (new String ("AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn")).concat(input).toCharArray();
 		    String  randomstr = RandomStringUtils.random(n, possibleCaracter);
-	        return randomstr;  
+		    System.out.println(randomstr);  
 		}} 
