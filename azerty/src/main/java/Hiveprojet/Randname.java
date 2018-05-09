0103e21bb95645f0b76db514dccb953de76117fe
Nisrine ELLOUMI <nel@softcomputing.com>
2018-05-09 11:40:26 +0200

    azerty the last

--- azerty/src/main/java/Hiveprojet/Randname.java
+++
@@ -1,9 +0,0 @@
-package Hiveprojet;
-
-import org.apache.hadoop.hive.ql.exec.UDF;
-
-public class Randname extends UDF {
-	
-	public static String evaluate(String input) {
-	    return( Projet.Randname.evaluate(input));  
-	}} 
